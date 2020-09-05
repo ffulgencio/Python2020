9f119e6db0857fc96c04b0b71c71fc6feb4c9eb2
Francis Fulgencio <francisfulgencio@gmail.com>
2020-09-04 20:19:39 -0400

    Para agregar el archivo ejemplo4.py

---
+++ d2-Ejemplo4/ejemplo4.py
@@ -0,0 +1,21 @@
+# # while (mientras)
+# c = 1
+# while c <= 10:
+#     if c % 2 == 0:
+#         print(c)
+#     c = c + 1
+
+# print("El bucle ha finalizado")
+
+# #tablas
+# n = input("Ingrese un numero para ver su tabla")
+# c = 1
+
+# while c <=12:
+#     res = int(n) * c
+#     print(f"{n} X {c} = {res}")
+#     c += 1 # c = c + 1
+
+# for
+for c in range(6):
+    print(c + 1)
